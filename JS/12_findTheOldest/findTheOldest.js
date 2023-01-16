03446cf3ce6d362502a019e6835fc09b5f8c96d2
Eddie Hakansson <eddan96@live.se>
2023-01-16 05:52:54 +0100

    changes

--- JS/12_findTheOldest/findTheOldest.js
+++ JS/12_findTheOldest/findTheOldest.js
@@ -1,12 +1,20 @@
 const findTheOldest = function(obj) {
-        obj.forEach(person => person.yearsLived = person.yearOfDeath - person.yearOfBirth);
-      
-        let sortedYearsLived = obj.sort(function(a,b){
-          let firstPerson = a.yearsLived;
-          let nextPerson = b.yearsLived;
-          return firstPerson > nextPerson ? -1:1;
-        });
-        return obj[0].name;
+  obj.forEach(person => person.yearsLived = person.yearOfDeath - person.yearOfBirth);
+
+  obj.sort(function(a,b){
+    return a.yearsLived > b.yearsLived ? -1:1;
+  })
+  let date = new Date()
+  let currentYear = date.getFullYear() 
+  return obj.reduce((oldest, currentPerson, index)=>{
+    if(oldest.yearsLived > currentPerson.yearsLived) {
+      return oldest;
+    }
+  else if(!currentPerson.yearOfDeath) {
+      return currentPerson;
+    }
+    return oldest;
+  }, obj[0]);
 };
 
 // Do not edit below this line
